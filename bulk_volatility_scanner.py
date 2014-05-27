295147090b64da19e7ac3b51ec238f905f264b75
gman2020 <mtgfeller@gmail.com>
2014-05-27 08:16:22 -0500

    Update bulk_volatility_scanner.py

--- bulk_volatility_scanner.py
+++ bulk_volatility_scanner.py
@@ -8,6 +8,8 @@ import os.path
 #argparse module allows the user to provide -h or --help for help messages
 parser = argparse.ArgumentParser(description="Subject a target memory image to a battery of volatility plugins.", epilog="The plugin file contains line-delimited Volatility plugin commands.")
 parser.add_argument("imagefile", help="Memory image")
+parser.add_argument("destfolder", help="Destination folder")
+parser.add_argument("name", help="Name of device")
 parser.add_argument("profile", help="Volatility profile")
 parser.add_argument("pluginfile", help="Plugin file")
 args = parser.parse_args()
@@ -17,12 +19,15 @@ args = parser.parse_args()
 #profile is the volatility memory profile
 #plugin_file is a line-delimited unix text file containing volatility plugin names
 image_file = args.imagefile
+dest_folder = args.destfolder
+name_file = args.name
 profile = args.profile
 plugin_file = args.pluginfile
 
 #welcome message
 print "Bulk Volatility Scanner v0.0.1"
 print "by Ryan Cobb 1/12/2014\n"
+print "modified by Matt Gfeller"
 print "Target Image File is %s" % image_file
 print "Plugin Configuration File is %s" % plugin_file
 print "The following plugins will be processed against the target image file."
@@ -36,13 +41,13 @@ print "Executing plugin battery on %s" % image_file
 try:
 
 #sets and creates directory filename based on image filename
-        directory_filename = image_file + "_" + "output"
+        directory_filename = dest_folder + "_" + "output"
         os.mkdir(directory_filename)
 
 #primary execution loop for plugins
-        for plugin in open(plugin_file):
+for plugin in open(plugin_file):
                 print "Plugin: " + plugin.rstrip() + " in progress"
-                output_filename = plugin.rstrip() + ".txt"
+                output_filename = name_file + "_" + plugin.rstrip() + ".txt"
                 output_file = open(os.path.join(directory_filename, output_filename), "w")
                 subprocess.call(["vol.py", "-f", image_file, "--profile="+profile, plugin.rstrip()], stdout=output_file)
                 print "Plugin: " + plugin.rstrip() + " completed"
